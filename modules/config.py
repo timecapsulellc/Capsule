1bfdd4f27d540ba283e032fae411441e43f4dbf1
Capsule LLC <dev@capsule.com>
2025-05-24 14:11:20 +0000

    Fix default styles and remove duplicated directories

--- modules/config.py
+++ modules/config.py
@@ -375,13 +375,7 @@ default_vae = get_config_item_or_set_default(
 )
 default_styles = get_config_item_or_set_default(
     key='default_styles',
-    default_value=[
-        "Capsule V2",
-        "Capsule Enhance",
-        "Capsule Sharp"
-    ],
-    validator=lambda x: isinstance(x, list) and all(y in modules.sdxl_styles.legal_style_names for y in x),
-    expected_type=list
+    default=["Capsule V2", "Capsule Enhance", "Capsule Sharp"]
 )
 default_prompt_negative = get_config_item_or_set_default(
     key='default_prompt_negative',
